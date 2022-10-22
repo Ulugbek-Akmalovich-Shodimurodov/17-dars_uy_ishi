c3b81f3e6bd8c81ad34dc011c84d436f0c5417a7
Ulugbek-Akmalovich-Shodimurodov <ulugbekshodimurodov16@gmail.com>
2022-10-22 20:46:55 +0500

    finish-code

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 
 let btn = document.querySelector('.clicked_btn')
 let popup = document.querySelector('.popup')
-let randomNumber = Math.round(Math.random()*20)
+let openMContainer = document.querySelector('.modal-container')
 let modalBtn = document.querySelector('.modal-btn');
+let modalTitle = document.querySelector('.modal-title')
+let modalImg = document.querySelector('.modal-img')
+let modalImg1 = document.querySelector('.modal-img1')
+
+let randomNumber = Math.round(Math.random()*25)
+
 
 modalBtn.addEventListener('click', ()=>{
     findNumber();
     popup.classList.remove('open-popup')
+    openMContainer.classList.remove('modal-container-open')
 } )
 
 function openModal(){
     popup.classList.add('open-popup')
+    openMContainer.classList.add('modal-container-open')
 }
 
 function findNumber(){
@@ -18,12 +26,16 @@ function findNumber(){
     let explanatoryText = document.querySelector('.explanatory-text')
     let chance = document.querySelector('.chance-number')
     let chanceValue = chance.textContent--;
-    console.log(randomNumber);
-    console.log(input.value);
-    if(chanceValue){
 
+    if(chanceValue){
         if(input.value - 0 == randomNumber){
+
+            modalTitle.textContent = 'You won !!!'
+            modalImg.classList.add('modal-img1')
+            modalImg1.classList.add('modal-img1-1')
+            popup.classList.add('open-popup')
             openModal()
+
         } else if(input.value - 0 < randomNumber){
             explanatoryText.textContent = 'The number entered is small'
         }else{
